eff12dbd84efcaadcc4885ce1dd86dde222ebdb6
Neemias Lacerda Leal <neemias.lacerda@yahoo.com.br>
2022-06-22 18:01:13 -0300

    updated to apply the private modifier to set function of cpf property

--- Modulo 2 Kotlin/fundamentosDeOOComKotlin/meuProjetoKotlin/meuProjeto/src/main/kotlin/meuProjeto/Pessoa.kt
+++ Modulo 2 Kotlin/fundamentosDeOOComKotlin/meuProjetoKotlin/meuProjeto/src/main/kotlin/meuProjeto/Pessoa.kt
@@ -2,11 +2,14 @@ package meuProjeto
 
 class Pessoa {
     var nome: String = "Neemias"
+
     var cpf: String = "123.123.123-11"
+    private set
+
 }
 
 fun main() {
-    val neemias = Pessoa ()
+    val neemias = Pessoa()
 
     println(neemias.nome)
     println(neemias.cpf)
