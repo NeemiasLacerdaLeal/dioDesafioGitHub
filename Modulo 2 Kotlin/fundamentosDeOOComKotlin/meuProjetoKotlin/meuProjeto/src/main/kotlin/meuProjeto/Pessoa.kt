cf731f08551b1d4bfa928cb2995a8d90339f41f9
Neemias Lacerda Leal <neemias.lacerda@yahoo.com.br>
2022-06-22 18:11:54 -0300

    updated to apply talk about class members

--- Modulo 2 Kotlin/fundamentosDeOOComKotlin/meuProjetoKotlin/meuProjeto/src/main/kotlin/meuProjeto/Pessoa.kt
+++ Modulo 2 Kotlin/fundamentosDeOOComKotlin/meuProjetoKotlin/meuProjeto/src/main/kotlin/meuProjeto/Pessoa.kt
@@ -2,15 +2,16 @@ package meuProjeto
 
 class Pessoa {
     var nome: String = "Neemias"
-
     var cpf: String = "123.123.123-11"
     private set
 
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val neemias = Pessoa()
 
-    println(neemias.nome)
-    println(neemias.cpf)
+    println(neemias.pessoaInfo())
 }
