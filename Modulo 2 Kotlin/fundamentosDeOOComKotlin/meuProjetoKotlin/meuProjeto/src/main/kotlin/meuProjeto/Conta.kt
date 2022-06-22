8393e7ff022e044c1c5bf89f09ccfc61405ae009
Neemias Lacerda Leal <neemias.lacerda@yahoo.com.br>
2022-06-22 18:23:46 -0300

    created account class

---
+++ Modulo 2 Kotlin/fundamentosDeOOComKotlin/meuProjetoKotlin/meuProjeto/src/main/kotlin/meuProjeto/Conta.kt
@@ -0,0 +1,17 @@
+package meuProjeto
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String
+    val numero: String
+    val Saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal)
+
+    }
+
+    fun saque(valor: BigDecimal) {
+
+    }
+}
